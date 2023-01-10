9e55d0a7a3d12167f44e5b0317f12c146bd35bd4
Adityaraje Nikrad <121033826+Adityaraje05@users.noreply.github.com>
2023-01-10 22:10:36 +0530

    Add files via upload

---
+++ pass.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int marks; 
+    printf("enter number (1-100): ");
+    scanf("%d" , &marks);
+    if("marks <= 30"){
+printf("fail \n");
+
+    }
+    else{
+        printf("PASS \n");
+    }
+   
+      return 0;
+    } 
